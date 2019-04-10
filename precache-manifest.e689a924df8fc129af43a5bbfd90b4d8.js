911e98f2184df099a8d2f0e5b5b66d8d224ed077
Shivam Rawat <shivamrawat6670@gmail.com>
2019-04-10 21:50:14 +0530

    Updates

---
+++ precache-manifest.e689a924df8fc129af43a5bbfd90b4d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "788e3d36e8c4d4120daeee6e57e7e85f",
+    "url": "/static/media/card-profile2-square.788e3d36.jpg"
+  },
+  {
+    "revision": "ab28a226b6f8d47d3761",
+    "url": "/static/css/main.36c1537d.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "5833df5bfaf12349c1b0707ae76b61dc",
+    "url": "/static/media/bg8.5833df5b.jpg"
+  },
+  {
+    "revision": "e23fd4549e9e611da9de",
+    "url": "/static/js/2.e23fd454.chunk.js"
+  },
+  {
+    "revision": "3122abf4a3e1067926c08dee7684522d",
+    "url": "/static/media/card-profile1-square.3122abf4.jpg"
+  },
+  {
+    "revision": "ab28a226b6f8d47d3761",
+    "url": "/static/js/main.ab28a226.chunk.js"
+  },
+  {
+    "revision": "1a1649171cdecd24e7a16ea1340077df",
+    "url": "/static/media/card-profile4-square.1a164917.jpg"
+  },
+  {
+    "revision": "1f1f4900f3bc166b75f8235c2b89864f",
+    "url": "/static/media/card-profile6-square.1f1f4900.jpg"
+  },
+  {
+    "revision": "013b04881a6177ff0648349533ef0447",
+    "url": "/static/media/bg7.013b0488.jpg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "e23fd4549e9e611da9de",
+    "url": "/static/css/2.9009c6d9.chunk.css"
+  },
+  {
+    "revision": "57436bc008910219f2c5384e43a9c864",
+    "url": "/index.html"
+  }
+];
